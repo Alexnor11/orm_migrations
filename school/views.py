aed615f72d474f9e9d16235e85f7c7eb32377ee0
Alexnor11 <advance114@gmail.com>
2022-01-05 14:18:20 +0300

    add sorting

--- school/views.py
+++ school/views.py
@@ -6,7 +6,7 @@ from .models import Student, Teacher
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
+    object_list = Student.objects.order_by('group')
 
     context = {
         'object_list': object_list,
